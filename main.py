e00cf311178217853493e095e9952fbce248692b
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-02-06 19:42:55 +0200

    2 task add

--- main.py
+++ main.py
@@ -35,3 +35,44 @@ class Suo(Gyvunas):
         print(f'{self.vardas} loja!!!')
 dog = Suo('Messi','Kreminis')
 dog.loti()
+print('-'*40)
+# Example
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, elem):
+        self.data.append(elem)
+
+    def pop(self):
+        return self.data.pop()
+
+class Element:
+    def __init__(self, item1, item2):
+        self.item1 = item1
+        self.item2 = item2
+
+    def __str__(self):
+        return f'{self.item1} {self.item2}'
+
+elems = [
+     Element(1,2),
+     Element(3,4),
+     Element(5,6)
+]
+
+stck = Stack()
+
+for i in elems:
+    stck.push(i)
+
+a = stck.pop()
+
+for i in stck.data:
+    print(i)
+print('-'*20)
+print(a)
+
+
+
+
--- tasks.py
+++ tasks.py
@@ -24,3 +24,25 @@ cat.miaukseti()
 dog.judeti()
 dog.lot()
 # dog.miaukseti() or cat.lot() is not working because functions in different classes
+print('-'*40)
+#  2 Task
+class Variklis:
+    def __init__(self,galia):
+        self.galia = galia
+    def startuoti(self):
+        print(f'Variklis veikia su galia: {self.galia} arklio galiu')
+#
+class Automobilis:
+    def __init__(self,marke,modelis,variklis):
+        self.marke = marke
+        self.modelis = modelis
+        self.variklis = variklis
+    def vaziuoti(self):
+        self.variklis.startuoti()
+#
+auto1 = Automobilis('BMW', 'M3', Variklis(500))
+auto2 = Automobilis('Tesla', 'Model S Plaid', Variklis(1020))
+auto1.vaziuoti()
+auto2.vaziuoti()
+
+
